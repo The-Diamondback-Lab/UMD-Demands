09435ee96e1596c2082342225f932df9df24d0ac
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-03 19:20:17 -0400

    Landing page background fits properly

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import landingPageImg from './Assets/homepageBackground.jpg';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-        <img style={{ display: 'flex' , width: '100%', height: '100%' }} src={landingPageImg} />;
-        <h1>UMD - Black Lives Matter - List of Demands</h1>
+      <div id="landing">
+        {/* <h1>UMD - Black Lives Matter - List of Demands</h1> */}
       </div>
     )
   }
