0ef3f2916eac8093012122ed7c5a8085ed189197
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-04 16:55:49 -0400

    Added main title

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -6,7 +6,9 @@ class LandingPage extends Component {
   render() {
     return (
       <div id="landing">
-        {/* <h1>UMD - Black Lives Matter - List of Demands</h1> */}
+        <div id="landing-title-container">
+          <h1>DEMANDS</h1>
+        </div>
       </div>
     )
   }
