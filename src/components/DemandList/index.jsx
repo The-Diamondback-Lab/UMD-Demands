c1185480ce19f4b2ede4d56b27c4e6263a33450d
Vishal Patel <vishalpatel3199@gmail.com>
2021-04-03 22:46:10 -0400

    Converted to demand list to class component

--- src/components/DemandList/index.jsx
+++ src/components/DemandList/index.jsx
@@ -6,28 +6,29 @@ import data from '../../data/undergraduate-demands.json';
 import './styles.css';
 
 import { LoremIpsum } from 'lorem-ipsum';
+import { Component } from 'react';
 
 const lorem = new LoremIpsum();
 
-export default function DemandList() {
-  let elems = data.map((o, i) => (
-    <div key={`UNDERGRADUATE-DEMAND-${i+1}`} className="demand-list item">
-      <Accordion>
-        <Card>
-          <Accordion.Toggle as={Card.Header} eventKey={`${i+1}`} className="title">
-            <div style={{ flex: 0.5 }}><img src={`/assets/numbericons/${i+1}.png`} style={{ width: '75%' }} /></div>
-            <div style={{ flex: .1 }}></div>
-            <div style={{ flex: 8, alignSelf: 'center', fontSize: 20 }}>{o.header}</div>
-          </Accordion.Toggle>
+export default class DemandList extends Component {
+  render() {
+    return data.map((o, i) => (
+      <div key={`UNDERGRADUATE-DEMAND-${i+1}`} className="demand-list item">
+        <Accordion>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey={`${i+1}`} className="title">
+              <div style={{ flex: 0.5 }}><img src={`/assets/numbericons/${i+1}.png`} style={{ width: '75%' }} /></div>
+              <div style={{ flex: .1 }}></div>
+              <div style={{ flex: 8, alignSelf: 'center', fontSize: 20 }}>{o.header}</div>
+            </Accordion.Toggle>
 
-          <Accordion.Collapse eventKey={`${i+1}`}>
-            <Card.Body dangerouslySetInnerHTML={{__html: o.body || lorem.generateSentences(10)}}>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-    </div>
-  ));
-
-  return elems;
+            <Accordion.Collapse eventKey={`${i+1}`}>
+              <Card.Body dangerouslySetInnerHTML={{__html: o.body || lorem.generateSentences(10)}}>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
+    ));
+  }
 }
